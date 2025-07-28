4eabaf5d7c1316ac18e96fac68b931b263bf92ac
Md Imam Hussain <mihussain737@gmail.com>
2025-07-28 21:05:03 +0530

    table with dummy data added

--- src/components/ListDepartmentsComponent.jsx
+++ src/components/ListDepartmentsComponent.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ListDepartmentsComponent = () => {
+
+     let  dummyData=[
+          {
+               "id":1,
+               "departmentName":"R&D",
+               "departmentDescription":"Research and Department"
+          },
+          {
+               "id":2,
+               "departmentName":"Finance",
+               "departmentDescription":"Finance Developement"
+          },
+          {
+               "id":3,
+               "departmentName":"Sports",
+               "departmentDescription":"Sports Department"
+          }
+     ]
+
+     const [departments,setDepartments]=useState(dummyData);
+
+     
   return (
-    <div>ListDepartmentsComponent</div>
+    <div className='container'>
+     <h2 className='text-center'>List of Departments</h2>
+     <table className='table table-bordered table-striped'>
+          <thead>
+               <tr>
+                    <th>Department Id</th>
+                    <th>Department Name</th>
+                    <th>Department Description</th>
+               </tr>
+          </thead>
+          <tbody>
+               {
+                    departments.map(department=>
+                         <tr key={department.id}>
+                              <td>{department.id}</td>
+                              <td>{department.departmentName}</td>
+                              <td>{department.departmentDescription}</td>
+                         </tr>
+                    )
+               }
+          </tbody>
+     </table>
+
+    </div>
   )
 }
 

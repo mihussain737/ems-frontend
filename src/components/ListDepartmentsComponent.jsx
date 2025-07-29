1cf040855256d7a8308f91e0bc82f5950bdc826c
Md Imam Hussain <mihussain737@gmail.com>
2025-07-29 22:39:12 +0530

    update and delete department button functionlity added

--- src/components/ListDepartmentsComponent.jsx
+++ src/components/ListDepartmentsComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { getAllDepartments } from '../services/DepartmentService';
-import { Link } from 'react-router-dom';
+import { deleteDepartment, getAllDepartments } from '../services/DepartmentService';
+import { Link, useNavigate } from 'react-router-dom';
+import { updateEmployee } from '../services/EmployeeService';
 
 const ListDepartmentsComponent = () => {
 
@@ -23,14 +24,35 @@ const ListDepartmentsComponent = () => {
      ] */
 
      const [departments,setDepartments]=useState([]);
+
+     const navigator=useNavigate();
+
      useEffect( ()=>{
+          listOfDepartments();
+     },[])
+
+     function listOfDepartments(){
           getAllDepartments().then((response)=>{
                console.log(response.data);
                setDepartments(response.data);
           }).catch(error=>{
                console.error(error)
           })
-     },[])
+     }
+
+     function updateDepartment(id){
+          navigator(`/edit-department/${id}`)
+     }
+     
+
+     function removeDepartment(id){
+          deleteDepartment(id).then((response)=>{
+               console.log(response.data)
+               listOfDepartments();
+          }).catch(error=>{
+               console.error(error)
+          })
+     }
 
      
   return (
@@ -43,6 +65,7 @@ const ListDepartmentsComponent = () => {
                     <th>Department Id</th>
                     <th>Department Name</th>
                     <th>Department Description</th>
+                    <th style={{textAlign:'center'}}>Actions</th>
                </tr>
           </thead>
           <tbody>
@@ -52,6 +75,10 @@ const ListDepartmentsComponent = () => {
                               <td>{department.id}</td>
                               <td>{department.departmentName}</td>
                               <td>{department.departmentDescription}</td>
+                              <td>
+                                   <button className='btn btn-info' onClick={()=>updateDepartment(department.id)}>Update</button>
+                                   <button className='btn btn-danger' onClick={()=>removeDepartment(department.id)} style={{marginLeft:'10px'}}>Delete</button>
+                              </td>
                          </tr>
                     )
                }
